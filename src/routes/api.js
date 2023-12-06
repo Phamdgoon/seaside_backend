5b59a3e244277163ceb6e417e3452c91ee963ff5
Phamdgoon <phamdgoon1606@gmail.com>
2023-12-06 18:33:38 +0700

    api-login

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,10 @@
 import express from "express";
-
+import LoginRegisterController from "../controller/LoginRegisterController";
 const router = express.Router();
 
 const initApiRoutes = (app) => {
+    router.post("/login", LoginRegisterController.handleLogin);
+
     return app.use("/", router);
 };
 
---
+++ src/services/LoginRegisterService.js
@@ -0,0 +1,102 @@
+import db from "../models/index";
+import mysql from "mysql2/promise";
+import bcrypt from "bcryptjs";
+const salt = bcrypt.genSaltSync(10);
+require("dotenv").config();
+
+// const hashUserPassword = (password) => {
+//     let hashPassword = bcrypt.hashSync(password, salt);
+//     return hashPassword;
+// };
+
+const checkPassword = (inputPassword, hashPassword) => {
+    return bcrypt.compareSync(inputPassword, hashPassword); // true or false
+};
+
+const handleLoginService = async (data) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT,
+        });
+
+        try {
+            const [userRows] = await connection.execute(
+                "SELECT * FROM user WHERE email = ?",
+                [data.email]
+            );
+
+            if (userRows.length > 0) {
+                const user = userRows[0];
+                const isCorrectPassword = checkPassword(
+                    data.password,
+                    userRows[0].password
+                );
+
+                const [permissionRows] = await connection.execute(
+                    "SELECT * FROM user_permission WHERE username = ?",
+                    [user.username]
+                );
+
+                if (isCorrectPassword && permissionRows.length > 0) {
+                    const permissions = permissionRows.map(
+                        (permissionRow) => permissionRow.id_permission
+                    );
+
+                    const permissionNames = await Promise.all(
+                        permissions.map(async (permissionId) => {
+                            const [permissionDetailRows] =
+                                await connection.execute(
+                                    "SELECT name_permission FROM permission WHERE id = ?",
+                                    [permissionId]
+                                );
+                            return permissionDetailRows[0].name_permission;
+                        })
+                    );
+
+                    const firstPermissionName = permissionNames[0];
+                    return {
+                        EM: `Đã đăng nhập với quyền ${firstPermissionName}`,
+                        EC: 0,
+                        DT: "",
+                    };
+                } else {
+                    return {
+                        EM: "Your email or password is incorrect!",
+                        EC: 1,
+                        DT: "",
+                    };
+                }
+            } else {
+                return {
+                    EM: "Your email or password is incorrect!",
+                    EC: 1,
+                    DT: "",
+                };
+            }
+        } catch (error) {
+            console.log(error);
+            return {
+                EM: "Error while executing query.",
+                EC: -3,
+                DT: "",
+            };
+        } finally {
+            connection.end();
+        }
+    } catch (e) {
+        console.error(e);
+        return {
+            EM: "There's something wrong with the service...",
+            EC: -2,
+            DT: "",
+        };
+    }
+};
+
+module.exports = {
+    handleLoginService,
+};
